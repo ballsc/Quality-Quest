03a72b63aaf075d39b5b87140566a4bb15a0f038
ballsc <145720840+ballsc@users.noreply.github.com>
2025-04-14 09:24:09 -0500

    Updated autonomy with test steer numbers

--- RoverAutonomy/avoidancePC_1.1.py
+++ RoverAutonomy/avoidancePC_1.1.py
@@ -4,7 +4,9 @@ import pyzed.sl as sl
 import math
 import numpy as np
 import sys
-import math
+import time
+
+# from pymavlink import mavutil
 
 MAX_U = 450 ## between 0 and 719
 MIN_U = 160
@@ -12,11 +14,48 @@ MAX_V = 750 ## between 0 and 1279
 MIN_V = 530
 
 STATUS = 0
- 
-## OVERALL TODO:
-## Calibrate Magnetometer
-## Make heading function using magnetometer for use in avoidObstacle
-## Finish avoidObstacle with dronekit and pymavlink functions
+
+TURN_DELAY = .5 #seconds
+
+# Constants for RC values
+RC_NEUTRAL = 1500
+RC_MIN = 1000
+RC_MAX = 2000
+
+# Connection parameters
+PORT = "COM5"  # Adjust as per your CubePilot's port
+BAUDRATE = 115200
+
+# # Connect to the CubePilot Rover
+# print("Connecting to CubePilot Rover...")
+# master = mavutil.mavlink_connection(PORT, baud=BAUDRATE)
+
+# # Wait for heartbeat
+# print("Waiting for heartbeat...")
+# master.wait_heartbeat()
+# print(f"Connected to system {master.target_system}, component {master.target_component}")
+
+# def send_rc_command(left_motor, right_motor, propeller_1=1500, propeller_2=1500):
+#     """
+#     Sends RC override commands to the specified channels.
+#     """
+#     # Enforce throttle limits
+#     left_motor = max(min(left_motor, RC_MAX), RC_MIN)
+#     right_motor = max(min(right_motor, RC_MAX), RC_MIN)
+#     propeller_1 = max(min(propeller_1, RC_MAX), RC_MIN)
+#     propeller_2 = max(min(propeller_2, RC_MAX), RC_MIN)
+
+#     # Send RC override command
+#     master.mav.rc_channels_override_send(
+#         master.target_system,
+#         master.target_component,
+#         left_motor,  # Channel 1 - Left motor
+#         right_motor, # Channel 3 - Right motor
+#         propeller_1, # Channel 4 - Propeller 1
+#         0,           # Channel 5 - Unused
+#         propeller_2, # Channel 6 - Propeller 2
+#         0, 0, 0, 0   # Channels 7 to 10 - Unused
+#     )
 
 def zedSetup():
 
@@ -44,7 +83,7 @@ def avoidObstacle():
   #     15  # GUIDED mode for ArduRover typically mode ID 15
   # )
 
-  # # Confirm mode change
+  # Confirm mode change
   # ack = False
   # while not ack:
   #     ack_msg = master.recv_match(type='HEARTBEAT', blocking=True, timeout=3)
@@ -52,35 +91,21 @@ def avoidObstacle():
   #         ack = True
   # print("Now in GUIDED mode")
 
-  # master.mav.rc_channels_override_send(
-  #   master.target_system,         # target_system (usually 1)
-  #   master.target_component,      # target_component (usually 1)
-  #   1100,            # Channel 1: Steering (Left) * 1100 left, 1500 typical, 1900 right
-  #   1500,                 # Channel 2: Throttle 1100 low, 1500 typical, 1900 high
-  #   0, 0, 0, 0, 0, 0              # Channels 3-8 unused (set 0 to ignore)
-  # )
+  # turn right
+  # send_rc_command(RC_NEUTRAL+100, RC_NEUTRAL-100)
+  # time.sleep(TURN_DELAY) # test how long to turn 90 deg, set TURN_DELAY to this
 
-  # delay(TURN_DELAY) # test how long to turn 90 deg, set TURN_DELAY to this
+  # # continue straight
+  # send_rc_command(RC_NEUTRAL, RC_NEUTRAL)
+  # time.sleep(.3) #seconds
 
-  # master.mav.rc_channels_override_send(
-  #   master.target_system,         # target_system (usually 1)
-  #   master.target_component,      # target_component (usually 1)
-  #   1900,            # Channel 1: Steering (Left) * 1100 left, 1500 typical, 1900 right
-  #   1500,                 # Channel 2: Throttle 1100 low, 1500 typical, 1900 high
-  #   0, 0, 0, 0, 0, 0              # Channels 3-8 unused (set 0 to ignore)
-  # )
+  # # turn left
+  # send_rc_command(RC_NEUTRAL-100, RC_NEUTRAL+100)
+  # time.sleep(TURN_DELAY) # test how long to turn 90 deg, set TURN_DELAY to this
 
-  # delay(TURN_DELAY) # test how long to turn 90 deg, set TURN_DELAY to this
+  # send_rc_command(RC_NEUTRAL, RC_NEUTRAL)
 
-  # master.mav.rc_channels_override_send(
-  #   master.target_system,         # target_system (usually 1)
-  #   master.target_component,      # target_component (usually 1)
-  #   1500,            # Channel 1: Steering (Left) * 1100 left, 1500 typical, 1900 right
-  #   1500,                 # Channel 2: Throttle 1100 low, 1500 typical, 1900 high
-  #   0, 0, 0, 0, 0, 0              # Channels 3-8 unused (set 0 to ignore)
-  # )
-
-  # Switch back to AUTO mode
+  # # Switch back to AUTO mode
   # master.mav.set_mode_send(
   #     master.target_system,
   #     mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
@@ -93,12 +118,6 @@ def unavaliable():
   while(1):
     STATUS = 0
 
-def currHeading():
-  # N NE E SE S SW W NW
-  heading = "N"
-  ## TODO: FINISH with magenetometer
-
-  return heading
 
 def main():
 
